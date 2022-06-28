fe204636c565e6e203c3a5fac45942702aac029e
R7trztrz <R7trztrz@outlook.com>
2022-06-28 20:09:01 +0800

    项目框架搭建

---
+++ HotelPMS/src/test/java/com/hotelpms/HotelPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelpms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelPmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
