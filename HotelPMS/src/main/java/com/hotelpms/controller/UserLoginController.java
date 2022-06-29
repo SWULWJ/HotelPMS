b626e75a009c79f47624fb5d119c1a0cb25d1b95
R7trztrz <R7trztrz@outlook.com>
2022-06-29 16:39:59 +0800

    登陆功能添加拦截器（未连接数据库）
    顺手增加了显示用户名功能

--- HotelPMS/src/main/java/com/hotelpms/controller/UserLoginController.java
+++ HotelPMS/src/main/java/com/hotelpms/controller/UserLoginController.java
@@ -5,18 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class UserLoginController {
     @RequestMapping("/userLogin")
     public String userLogin(
             @RequestParam("username")String username,
             @RequestParam("password")String password,
-            Model model) {
+            Model model,
+            HttpSession session) {
 
         //放入service
-        if(username.equals("admin") && password.equals("admin"))
+        if(username.equals("admin") && password.equals("admin")) {
+            session.setAttribute("loginUser", username);
             return "redirect:/system.html";
-        else {
+        } else {
             model.addAttribute("msg", "用户名或密码错误");
             return "login";
         }
