1dc2d5dedce14d240afe29775f56c643ea5ea1ba
R7trztrz <R7trztrz@outlook.com>
2022-06-30 15:12:09 +0800

    完成所有实体类

---
+++ HotelPMS/src/main/java/com/hotelpms/pojo/DispatchRecords.java
@@ -0,0 +1,21 @@
+package com.hotelpms.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+//派遣表
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DispatchRecords {
+    private int id;
+    private int staffId;
+    private String location;
+    private Date startTime;
+    private Date endTime;
+    private String type;
+    private String details;
+}
