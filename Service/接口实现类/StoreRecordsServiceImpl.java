5a7b43d3532e6fb9619d492e6b8d3d9babbec2ab
Vacat10n <108106733+Vacat10n@users.noreply.github.com>
2022-07-02 17:43:28 +0800

    StoreRecords实现类，TenantInfo实现类

---
+++ Service/接口实现类/StoreRecordsServiceImpl.java
@@ -0,0 +1,53 @@
+package com.hotelpms.service.finish;
+
+import com.hotelpms.mapper.StoreRecordsMapper;
+import com.hotelpms.pojo.StoreRecords;
+import com.hotelpms.service.finish.StoreRecordsService;
+import org.apache.catalina.Store;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class StoreRecordsServiceImpl implements StoreRecordsService {
+
+    @Autowired
+    StoreRecordsMapper storeRecordsMapper;
+
+    @Override
+    public StoreRecords queryStoreRecordsById(int id) {
+        return storeRecordsMapper.queryStoreRecordsById(id);
+    }
+
+    @Override
+    public List<StoreRecords> queryAllStoreRecords() {
+        return storeRecordsMapper.queryAllStoreRecords();
+    }
+
+    @Override
+    public boolean addStoreRecords(int id, String name, int amount, BigDecimal price) {
+        if(storeRecordsMapper.queryStoreRecordsById(id) == null){
+            StoreRecords storeRecords = new StoreRecords( id , name , amount , price );
+            storeRecordsMapper.addStoreRecords(storeRecords);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteStoreRecords(int id) {
+        if(storeRecordsMapper.queryStoreRecordsById(id) != null){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateStoreRecords(int id, String name, int amount, BigDecimal price) {
+        StoreRecords storeRecords = new StoreRecords( id , name , amount , price );
+        storeRecordsMapper.updateStoreRecords(storeRecords);
+        return false;
+    }
+}
