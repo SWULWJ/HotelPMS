7ac89485b4cbf463081446408f5c5b78fe685734
R7trztrz <R7trztrz@outlook.com>
2022-07-02 10:08:29 +0800

    简单表接口

--- HotelPMS/src/main/java/com/hotelpms/mapper/StoreRecordsMapper.java
+++ HotelPMS/src/main/java/com/hotelpms/mapper/StoreRecordsMapper.java
@@ -1,4 +1,30 @@
 package com.hotelpms.mapper;
 
+import com.hotelpms.pojo.RoomType;
+import com.hotelpms.pojo.StoreRecords;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
 public interface StoreRecordsMapper {
+
+    //新增一条库存记录
+    void addStoreRecords(StoreRecords storeRecords);
+
+    //通过id删除一条库存记录
+    void deleteStoreRecords(int id);
+
+    //更新一条库存记录
+    void updateStoreRecords(StoreRecords storeRecords);
+
+    //通过id查找一条库存记录
+    RoomType queryStoreRecordsById(int id);
+
+    //通过物品名称查找一条库存记录
+    RoomType queryStoreRecordsByName();
+
+    //查找所有库存记录
+    RoomType queryAllStoreRecords();
+
 }
---
+++ HotelPMS/src/main/java/com/hotelpms/service/RoomTypeImpl.java
@@ -0,0 +1,35 @@
+package com.hotelpms.service;
+
+import com.hotelpms.pojo.RoomType;
+
+public class RoomTypeImpl implements RoomTypeService{
+    @Override
+    public boolean addRoomType(RoomType roomType) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteRoomType(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean updateRoomType(RoomType roomType) {
+        return false;
+    }
+
+    @Override
+    public RoomType queryRoomTypeById(int id) {
+        return null;
+    }
+
+    @Override
+    public RoomType queryRoomTypeByName(String name) {
+        return null;
+    }
+
+    @Override
+    public RoomType queryAllRoomType() {
+        return null;
+    }
+}
