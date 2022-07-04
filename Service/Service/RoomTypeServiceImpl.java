e586cad11c7b29dbafa937db8b1eb84c86eff7ba
Vacat10n <108106733+Vacat10n@users.noreply.github.com>
2022-07-04 14:37:53 +0800

    roomtype，storerecords，tenantinfo新增通过name查询的方法

---
+++ Service/Service/RoomTypeServiceImpl.java
@@ -0,0 +1,64 @@
+package com.hotelpms.service.finish;
+
+import com.hotelpms.mapper.RoomTypeMapper;
+import com.hotelpms.pojo.RoomType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class RoomTypeServiceImpl implements RoomTypeService{
+
+    @Autowired
+    RoomTypeMapper roomTypeMapper;
+
+    @Override
+    public boolean addRoomType(int id, String name, BigDecimal price,
+                               int bed_number, int max_people, String min_time) {
+        if (roomTypeMapper.queryRoomTypeById(id) == null) {
+            RoomType roomType = new RoomType(id, name, price,
+                    bed_number, max_people, min_time);
+            roomTypeMapper.addRoomType(roomType);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteRoomTypeById(int id) {
+        if (roomTypeMapper.queryRoomTypeById(id) != null) {
+            roomTypeMapper.deleteRoomType(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateRoomType(int id, String name, BigDecimal price,
+                                  int bed_number, int max_people, String min_time) {
+        if (roomTypeMapper.queryRoomTypeById(id) == null){
+            RoomType roomType = new RoomType(id, name, price,
+                    bed_number, max_people, min_time);
+            roomTypeMapper.updateRoomType(roomType);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public RoomType queryRoomTypeById(int id) {
+        return roomTypeMapper.queryRoomTypeById(id);
+    }
+
+    @Override
+    public RoomType queryRoomTypeByName(String name) {
+        return roomTypeMapper.queryRoomTypeByName(name);
+    }
+
+    @Override
+    public List<RoomType> queryAllRoomType() {
+        return roomTypeMapper.queryAllRoomType();
+    }
+}
