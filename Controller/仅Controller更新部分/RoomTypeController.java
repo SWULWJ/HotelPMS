b6f2237b5ba8e191a783d09d307e97d1832d112f
whitingsea <1257104733@qq.com>
2022-07-04 11:16:14 +0800

    按要求修改了格式

---
+++ Controller/仅Controller更新部分/RoomTypeController.java
@@ -0,0 +1,167 @@
+package com.hotelpms.controller;
+
+import com.hotelpms.pojo.RoomType;
+import com.hotelpms.service.Impl.RoomTypeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class RoomTypeController {
+
+    @Autowired
+    private RoomTypeServiceImpl roomTypeService;
+
+//    @RequestMapping(value = "/addRoomType",method = RequestMethod.GET)
+//    @ResponseBody
+//    public String addRoomType(
+//            @RequestParam("id") int id,
+//            @RequestParam("name") String name,
+//            @RequestParam("price") BigDecimal price,
+//            @RequestParam("bedNumber") int bedNumber,
+//            @RequestParam("maxPeople") int maxPeople,
+//            @RequestParam("minTime") String minTime
+//    ){
+//        if(roomTypeService.addRoomType(id, name, price, bedNumber, maxPeople, minTime))
+//            return "Success";
+//        return "Failed";
+//    }
+
+    @RequestMapping(value = "/addRoomType",method = RequestMethod.GET)
+    @ResponseBody
+    public String addRoomType(
+            @RequestParam("id") String id,
+            @RequestParam("name") String name,
+            @RequestParam("price") BigDecimal price,
+            @RequestParam("bedNumber") String bedNumber,
+            @RequestParam("maxPeople") String maxPeople,
+            @RequestParam("minTime") String minTime
+    ){
+        if(roomTypeService.addRoomType(Integer.parseInt(id),
+                name, price, Integer.parseInt(bedNumber),
+                Integer.parseInt(maxPeople), minTime))
+            return "Success";
+        return "Failed";
+    }
+
+//    @RequestMapping(value = "/updateRoomType",method = RequestMethod.GET)
+//    @ResponseBody
+//    public String updateRoomType(
+//            @RequestParam("id") int id,
+//            @RequestParam("name") String name,
+//            @RequestParam("price") BigDecimal price,
+//            @RequestParam("bedNumber") int bedNumber,
+//            @RequestParam("maxPeople") int maxPeople,
+//            @RequestParam("minTime") String minTime
+//    ){
+//        if(roomTypeService.updateRoomType(id, name, price, bedNumber, maxPeople, minTime))
+//            return "Success";
+//        return "Failed";
+//    }
+
+    @RequestMapping(value = "/updateRoomType",method = RequestMethod.GET)
+    @ResponseBody
+    public String updateRoomType(
+            @RequestParam("id") String id,
+            @RequestParam("name") String name,
+            @RequestParam("price") String price,
+            @RequestParam("bedNumber") String bedNumber,
+            @RequestParam("maxPeople") String maxPeople,
+            @RequestParam("minTime") String minTime
+    ){
+        if(roomTypeService.updateRoomType(Integer.parseInt(id),
+                name, new BigDecimal(price), Integer.parseInt(bedNumber),
+                Integer.parseInt(maxPeople), minTime))
+            return "Success";
+        return "Failed";
+    }
+
+//    @RequestMapping(value = "/deleteRoomTypeById",method = RequestMethod.GET)
+//    @ResponseBody
+//    public String deleteRoomTypeById(
+//            @RequestParam("id") String id
+//    ){
+//        if(roomTypeService.deleteRoomTypeById(id))
+//            return "Success";
+//        return "Failed";
+//    }
+
+    @RequestMapping(value = "/deleteRoomTypeById",method = RequestMethod.GET)
+    @ResponseBody
+    public String deleteRoomTypeById(
+            @RequestParam("id") String id
+    ){
+        if(roomTypeService.deleteRoomTypeById(Integer.parseInt(id)))
+            return "Success";
+        return "Failed";
+    }
+
+//    @RequestMapping(value = "/queryRoomTypeById",method = RequestMethod.GET)
+//    @ResponseBody
+//    public RoomType queryRoomTypeById(
+//            @RequestParam("id") int id
+//    ){
+//        return roomTypeService.queryRoomTypeById(id);
+//    }
+
+    @RequestMapping(value = "/queryRoomTypeById",method = RequestMethod.GET)
+    @ResponseBody
+    public RoomType queryRoomTypeById(
+            @RequestParam("id") String id
+    ){
+        return roomTypeService.queryRoomTypeById(Integer.parseInt(id));
+    }
+
+//    @RequestMapping(value = "/QueryRoomTypeByPrice",method = RequestMethod.GET)
+//    @ResponseBody
+//    public List<RoomType> queryRoomTypeByPrice(
+//            @RequestParam("min_price") BigDecimal min_price,
+//            @RequestParam("max_price") BigDecimal max_price
+//    ){
+//        return roomTypeService.queryRoomTypeByPrice(min_price,max_price);
+//    }
+
+    @RequestMapping(value = "/queryRoomTypeByMaxPeople",method = RequestMethod.GET)
+    @ResponseBody
+    public List<RoomType> queryRoomTypeByMaxPeople(
+            @RequestParam("maxPeople") String maxPeople
+    ){
+        return roomTypeService.queryRoomTypeByMaxPeople(
+                Integer.parseInt(maxPeople));
+    }
+
+    @RequestMapping(value = "/queryRoomTypeByMinTime",method = RequestMethod.GET)
+    @ResponseBody
+    public List<RoomType> queryRoomTypeByMinTime(
+            @RequestParam("minTime") String minTime
+    ){
+        return roomTypeService.queryRoomTypeByMinTime(minTime);
+    }
+
+//    @RequestMapping(value = "/queryRoomTypeByBedNum",method = RequestMethod.GET)
+//    @ResponseBody
+//    public List<RoomType> queryRoomTypeByBedNum(
+//            @RequestParam("bedNumber") int bedNumber
+//    ){
+//        return roomTypeService.queryRoomTypeByBedNum(bedNumber);
+//    }
+
+    @RequestMapping(value = "/queryRoomTypeByBedNum",method = RequestMethod.GET)
+    @ResponseBody
+    public List<RoomType> queryRoomTypeByBedNum(
+            @RequestParam("bedNumber") String bedNumber
+    ){
+        return roomTypeService.queryRoomTypeByBedNum(
+                Integer.parseInt(bedNumber));
+    }
+
+    @RequestMapping(value = "/queryAllRoomType",method = RequestMethod.GET)
+    @ResponseBody
+    public List<RoomType> queryAllRoomType(){
+        return roomTypeService.queryAllRoomType();
+    }
+}
---
+++ Controller/仅Controller更新部分/StaffInfoController.java
@@ -0,0 +1,124 @@
+package com.hotelpms.controller;
+
+import com.hotelpms.pojo.StaffInfo;
+import com.hotelpms.service.Impl.StaffInfoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+public class StaffInfoController {
+
+    @Autowired
+    private StaffInfoServiceImpl staffInfoService;
+
+//    @RequestMapping(value = "/UpdateStaff",method = RequestMethod.GET)
+//    @ResponseBody
+//    public String UpdateStuff(
+//            @RequestParam("id") int id,
+//            @RequestParam("staffNumber") int sn,
+//            @RequestParam("name") String name,
+//            @RequestParam("gender") String gender,
+//            @RequestParam("age") int age,
+//            @RequestParam("position") String pos,
+//            @RequestParam("tel") String tel) {
+//        if (staffInfoService.updateStaffInfo(id, sn, name, gender, age, pos, tel))
+//            return "Success";
+//        return "False";
+//    }
+
+    @RequestMapping(value = "/updateStaff",method = RequestMethod.GET)
+    @ResponseBody
+    public String UpdateStuff(
+            @RequestParam("id") String id,
+            @RequestParam("staffNumber") String sn,
+            @RequestParam("name") String name,
+            @RequestParam("gender") String gender,
+            @RequestParam("age") String age,
+            @RequestParam("position") String pos,
+            @RequestParam("tel") String tel) {
+        if (staffInfoService.updateStaffInfo(Integer.parseInt(id), Integer.parseInt(sn),
+                name, gender, Integer.parseInt(age), pos, tel))
+            return "Success";
+        return "Failed";
+    }
+
+//    @RequestMapping(value = "/AddStaff",method = RequestMethod.GET)
+//    @ResponseBody
+//    public String AddStuff(
+//            @RequestParam("id") int id,
+//            @RequestParam("staffNumber") int sn,
+//            @RequestParam("name") String name,
+//            @RequestParam("gender") String gender,
+//            @RequestParam("age") int age,
+//            @RequestParam("position") String pos,
+//            @RequestParam("tel") String tel) {
+//        if (staffInfoService.addStaff(id, sn, name, gender, age, pos, tel))
+//            return "Success";
+//        return "Failed";
+//    }
+
+    @RequestMapping(value = "/addStaff",method = RequestMethod.GET)
+    @ResponseBody
+    public String AddStuff(
+            @RequestParam("id") String id,
+            @RequestParam("staffNumber") String sn,
+            @RequestParam("name") String name,
+            @RequestParam("gender") String gender,
+            @RequestParam("age") String age,
+            @RequestParam("position") String pos,
+            @RequestParam("tel") String tel) {
+        if (staffInfoService.addStaff(Integer.parseInt(id), Integer.parseInt(sn),
+                name, gender, Integer.parseInt(age), pos, tel))
+            return "Success";
+        return "Failed";
+    }
+
+//    @RequestMapping(value = "/DeleteStaff",method = RequestMethod.GET)
+//    @ResponseBody
+//    public String DeleteStaff(
+//            @RequestParam("id") int id){
+//        if (staffInfoService.deleteStaffInfoById(id))
+//            return "Success";
+//        return "False";
+//    }
+
+    @RequestMapping(value = "/deleteStaff",method = RequestMethod.GET)
+    @ResponseBody
+    public String DeleteStaff(
+            @RequestParam("id") String id){
+        if (staffInfoService.deleteStaffInfoById(Integer.parseInt(id)))
+            return "Success";
+        return "Failed";
+    }
+
+//    @RequestMapping(value = "/QueryStuffById",method = RequestMethod.GET)
+//    @ResponseBody
+//    public StaffInfo QueryStuffById(
+//            @RequestParam("id") int id){
+//        return staffInfoService.queryStaffInfoById(id);
+//    }
+
+    @RequestMapping(value = "/queryStuffById",method = RequestMethod.GET)
+    @ResponseBody
+    public StaffInfo QueryStuffById(
+            @RequestParam("id") String id){
+        return staffInfoService.queryStaffInfoById(Integer.parseInt(id));
+    }
+
+    @RequestMapping(value = "queryStuffByName",method = RequestMethod.GET)
+    @ResponseBody
+    public List<StaffInfo> QueryStuffByName(
+            @RequestParam("/name") String name){
+        return staffInfoService.queryStaffInfoByName(name);
+    }
+
+    @RequestMapping(value = "/queryAllStaff",method = RequestMethod.GET)
+    @ResponseBody
+    public List<StaffInfo> QueryAllStaff(){
+        return staffInfoService.queryAllStaffInfo();
+    }
+}
